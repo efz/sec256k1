d7fb3ac9a3eb47ec61b1a82162858c3f6a5758b6
iranth <iranthas@icloud.com>
2022-06-01 09:18:11 -0400

    cleanup

--- Sources/secp256k1s/Scalar.swift
+++ Sources/secp256k1s/Scalar.swift
@@ -11,45 +11,6 @@ public struct Secpt256k1Scalar {
     static let pComp: [UInt64] = [~p[0] + 1, ~p[1], ~p[2]]
     static let pCompLeadingZeros = 127
     
-    private static let pMinus2MetaData = { () -> (segmentDefs: [(start: Int, end: Int)], segments: [Int:Secpt256k1Scalar], maxSegmentLen: Int) in
-        var segmentDefs: [(start: Int, end: Int)] = []
-        var segments: [Int:Secpt256k1Scalar] = [:]
-        var maxSegmentLen = 0
-        var curSegStart = -1
-        let onNewSegment: (Int) -> Void = { end in
-            segmentDefs.append((curSegStart, end))
-            let segmentLen = end - curSegStart
-            segments[segmentLen] = Secpt256k1Scalar.zero
-            if segmentLen > maxSegmentLen {
-                maxSegmentLen = segmentLen
-            }
-            curSegStart = -1
-        }
-        
-        for i in 0..<Secpt256k1Scalar.wordWidth {
-            var bitMask: UInt64 = 1
-            for j in 0..<Secpt256k1Scalar.wordBitWidth {
-                let bitPos = i * Secpt256k1Scalar.wordBitWidth + j
-                
-                let bitSet = Secpt256k1Scalar.pMinus2[i] & bitMask != 0
-                if bitSet {
-                    if curSegStart == -1 {
-                        curSegStart = bitPos
-                    }
-                } else if curSegStart != -1 {
-                    onNewSegment(bitPos)
-                }
-                
-                bitMask = bitMask << 1
-            }
-        }
-        if curSegStart != -1 {
-            let endPos = Secpt256k1Scalar.wordWidth * Secpt256k1Scalar.wordBitWidth
-            onNewSegment(endPos)
-        }
-        return (segmentDefs, segments, maxSegmentLen)
-    }()
-    
     public static let zero = Secpt256k1Scalar()
     public static let one = Secpt256k1Scalar(int: 1)
     
@@ -60,8 +21,8 @@ public struct Secpt256k1Scalar {
     static let wordMask = UInt64.max
     
     public init() {
-        d = [UInt64](repeating: 0, count: Secpt256k1Scalar.wordWidth * 2)
-        bits512 = [UInt64](repeating: 0, count: Secpt256k1Scalar.wordWidth * 2)
+        d = [UInt64](repeating: 0, count: Secpt256k1Scalar.wordWidth * 4)
+        bits512 = [UInt64](repeating: 0, count: Secpt256k1Scalar.wordWidth * 4)
     }
     
     public init(words : [UInt32]) {
@@ -243,55 +204,6 @@ public struct Secpt256k1Scalar {
         return Secpt256k1Scalar.substract(x, y)
     }
     
-    static func reduceBitbyBit(bits512: inout [UInt64], from startPos: Int, to endPos: Int) {
-        assert(endPos >= Secpt256k1Scalar.wordBitWidth * Secpt256k1Scalar.wordWidth - 1)
-        assert(startPos >= endPos)
-        assert(bits512.count * Secpt256k1Scalar.wordBitWidth >= startPos)
-        
-        var bit = startPos
-        while bit > endPos {
-            let wordIdx = bit >> 6
-            let wordBitIdx = bit & 0x3F
-            let need2Clear = (bits512[wordIdx] & (1 << wordBitIdx)) >> wordBitIdx
-            assert(need2Clear <= 1)
-            
-            guard need2Clear > 0 else {
-                bit = Secpt256k1Scalar.wordBitWidth * wordIdx + Secpt256k1Scalar.wordBitWidth - bits512[wordIdx].leadingZeroBitCount - 1
-                continue
-            }
-            
-            let subStartBit = bit - Secpt256k1Scalar.wordWidth * Secpt256k1Scalar.wordBitWidth
-            let shift = subStartBit & 0x3F
-            var tmpdIdx = subStartBit >> 6
-            var t : UInt64 = 0
-            var overflow = false
-            
-            (bits512[tmpdIdx], overflow) = bits512[tmpdIdx].subtractingReportingOverflow((Secpt256k1Scalar.p[0] << shift))
-            t += overflow ? 1 : 0
-            tmpdIdx += 1
-            
-            for pIdx in 1..<Secpt256k1Scalar.wordWidth {
-                (bits512[tmpdIdx], overflow) = bits512[tmpdIdx].subtractingReportingOverflow(t)
-                t = overflow ? 1 : 0
-                (bits512[tmpdIdx], overflow) = bits512[tmpdIdx].subtractingReportingOverflow(
-                    (Secpt256k1Scalar.p[pIdx] << shift) | (Secpt256k1Scalar.p[pIdx - 1] >> (Secpt256k1Scalar.wordBitWidth - shift)))
-                t += overflow ? 1 : 0
-                tmpdIdx += 1
-            }
-            
-            (bits512[tmpdIdx], overflow) = bits512[tmpdIdx].subtractingReportingOverflow(t)
-            t = overflow ? 1 : 0
-            (bits512[tmpdIdx], overflow) = bits512[tmpdIdx].subtractingReportingOverflow(
-                Secpt256k1Scalar.p[Secpt256k1Scalar.wordWidth - 1] >> (Secpt256k1Scalar.wordBitWidth - shift))
-            t += overflow ? 1 : 0
-            
-            assert(t == 0 || (wordBitIdx == 0 && wordIdx == tmpdIdx + 1))
-            if t != 0 {
-                bits512[wordIdx] -= t
-            }
-        }
-    }
-    
     private mutating func reduceByPcomp() {
         let reductionPerRun = Secpt256k1Scalar.pCompLeadingZeros
         let reduceFromSize = Secpt256k1Scalar.wordWidth * 2
@@ -396,47 +308,6 @@ public struct Secpt256k1Scalar {
                 powers.sqr()
                 bitMask = bitMask << 1
             }
-        }
-    }
-    
-    private mutating func inverseBySegmentBuilding() {
-        var segments = Secpt256k1Scalar.pMinus2MetaData.segments
-        
-        // Build required segments
-        var seg = Secpt256k1Scalar(int: 1)
-        var x = Secpt256k1Scalar(scalar: self)
-        for i in 1..<Secpt256k1Scalar.pMinus2MetaData.maxSegmentLen+1 {
-            seg.mul(x)
-            if segments[i] != nil {
-                segments[i] = Secpt256k1Scalar(scalar: seg)
-            }
-            x.sqr()
-        }
-        
-        setInt(1)
-        assert(isOne())
-        
-        // Append segments to build in self^P-2
-        var lastSegStart = -1
-        for segDef in Secpt256k1Scalar.pMinus2MetaData.segmentDefs.reversed() {
-            if lastSegStart < segDef.end {// last segment
-                lastSegStart = segDef.end
-            }
-            
-            let segmentlen = segDef.end - segDef.start
-            
-            for _ in segDef.end..<lastSegStart + segmentlen {
-                sqr() // append 0s for segment seperation
-            }
-            
-            let segment = segments[segmentlen]!
-            assert(!segment.isZero())
-            mul(segment)
-            lastSegStart = segDef.start
-        }
-        
-        for _ in 0..<lastSegStart {
-            sqr() // append prefix 0s
         }
     }
     
