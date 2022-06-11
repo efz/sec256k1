380a636e0858e1a63be2fbd68af83d9e0a97c089
iranth <iranthas@icloud.com>
2022-06-10 23:09:04 -0400

    tuple based acc

--- Sources/secp256k1s/UInt256p.swift
+++ Sources/secp256k1s/UInt256p.swift
@@ -7,38 +7,39 @@ typealias Bits64x7 = (UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64)
 typealias Bits64x8 = (UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64)
 
 struct Accumulator {
-    var c0, c1, c2: UInt64
+    var c: (UInt64, UInt64, UInt64)
     
     init() {
-        (c0, c1, c2) = (0, 0, 0)
+        c = (0, 0, 0)
     }
     
     init(_ val: UInt64) {
-        (c0, c1, c2) = (val, 0, 0)
+        c = (val, 0, 0)
     }
     
+    @inline(__always)
     mutating func reset (_ val: UInt64) {
-        (c0, c1, c2) = (val, 0, 0)
+        c = (val, 0, 0)
     }
     
     @inline(__always)
     mutating func mulAddFast(_ x: UInt64, _ y: UInt64) {
         var overflow = false
         var (hi, lo) = x.multipliedFullWidth(by: y)
-        (c0, overflow) = c0.addingReportingOverflow(lo)
+        (c.0, overflow) = c.0.addingReportingOverflow(lo)
         hi += overflow ? 1 : 0
-        c1 += hi
-        assert(c2 == 0)
+        c.1 += hi
+        assert(c.2 == 0)
     }
     
     @inline(__always)
     mutating func mulAdd(_ x: UInt64, _ y: UInt64) {
         var overflow = false
         var (hi, lo) = x.multipliedFullWidth(by: y)
-        (c0, overflow) = c0.addingReportingOverflow(lo)
+        (c.0, overflow) = c.0.addingReportingOverflow(lo)
         hi += overflow ? 1 : 0
-        (c1, overflow) = c1.addingReportingOverflow(hi)
-        c2 += overflow ? 1 : 0
+        (c.1, overflow) = c.1.addingReportingOverflow(hi)
+        c.2 += overflow ? 1 : 0
     }
     
     @inline(__always)
@@ -46,51 +47,51 @@ struct Accumulator {
         var overflow = false
         var (hi, lo) = x.multipliedFullWidth(by: y)
         var hiCopy = hi
-        (c0, overflow) = c0.addingReportingOverflow(lo)
+        (c.0, overflow) = c.0.addingReportingOverflow(lo)
         hi += overflow ? 1 : 0
-        (c1, overflow) = c1.addingReportingOverflow(hi)
-        c2 += overflow ? 1 : 0
+        (c.1, overflow) = c.1.addingReportingOverflow(hi)
+        c.2 += overflow ? 1 : 0
         
-        (c0, overflow) = c0.addingReportingOverflow(lo)
+        (c.0, overflow) = c.0.addingReportingOverflow(lo)
         hiCopy += overflow ? 1 : 0
-        (c1, overflow) = c1.addingReportingOverflow(hiCopy)
-        c2 += overflow ? 1 : 0
+        (c.1, overflow) = c.1.addingReportingOverflow(hiCopy)
+        c.2 += overflow ? 1 : 0
     }
     
     @inline(__always)
     mutating func sumAdd(_ x: UInt64) {
         var overflow = false
-        (c0, overflow) = c0.addingReportingOverflow(x)
-        (c1, overflow) = c1.addingReportingOverflow(overflow ? 1 : 0)
-        c2 += overflow ? 1 : 0
+        (c.0, overflow) = c.0.addingReportingOverflow(x)
+        (c.1, overflow) = c.1.addingReportingOverflow(overflow ? 1 : 0)
+        c.2 += overflow ? 1 : 0
     }
     
     @inline(__always)
     mutating func sumAddFast(_ x: UInt64) {
         var overflow = false
-        (c0, overflow) = c0.addingReportingOverflow(x)
-        c1 += overflow ? 1 : 0
-        assert(c2 == 0)
+        (c.0, overflow) = c.0.addingReportingOverflow(x)
+        c.1 += overflow ? 1 : 0
+        assert(c.2 == 0)
     }
     
     @inline(__always)
     mutating func extractFast() -> UInt64 {
         defer {
-            (c0, c1) = (c1, 0)
-            assert(c2 == 0)
+            (c.0, c.1) = (c.1, 0)
+            assert(c.2 == 0)
         }
-        return c0
+        return c.0
     }
     
     @inline(__always)
     mutating func extract() -> UInt64 {
         defer {
-            (c0, c1, c2) = (c1, c2, 0)
+            c = (c.1, c.2, 0)
         }
-        return c0
+        return c.0
     }
     
     func isZero() -> Bool {
-        return (c0, c1, c2) == (0, 0, 0)
+        return c == (0, 0, 0)
     }
 }
